cbbd59989dfecc6c19d676dc41ce27696dbe6869
Bilshansakib <burakaghani@gmail.com>
2024-05-11 01:36:47 +0600

    single food data added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // jwt
 const jwt = require("jsonwebtoken");
 
@@ -38,6 +38,13 @@ async function run() {
     app.get("/foods", async (req, res) => {
       const result = await foodsCollection.find().toArray();
 
+      res.send(result);
+    });
+    // single food data
+    app.get("/food/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodsCollection.findOne(query);
       res.send(result);
     });
 
