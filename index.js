95e088fe323f3548f25ddc2e72ec0df24b64e1f2
Bilshansakib <burakaghani@gmail.com>
2024-05-15 23:58:31 +0600

    jwt

--- index.js
+++ index.js
@@ -4,26 +4,51 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // jwt
 const jwt = require("jsonwebtoken");
-
+const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 9000;
 const app = express();
 
 // middleware
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "https://assign11-client.web.app",
-    ],
-    credentials: true,
-    optionsSuccessStatus: 200,
-  })
-);
+const corsOption = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://assign11-client.web.app",
+    "https://assign11-client.firebaseapp.com",
+  ],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOption));
 app.use(express.json());
+app.use(cookieParser());
 console.log(process.env.DB_USER);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7hlvjai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+// jwt middleware
+// const logger = (req, res, next) => {
+//   console.log("log : info", req.method, req.url);
+//   next();
+// };
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log(token);
+
+  if (!token)
+    return res.status(401).send({ message: "You have unauthorized access" });
+  if (token) {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        console.log(err);
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      console.log(decoded);
+
+      req.user = decoded;
+      next();
+    });
+  }
+};
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -41,8 +66,41 @@ async function run() {
     const foodsCollection = client.db("myRestaurant").collection("foodItems");
     const orderCollection = client.db("myRestaurant").collection("orders");
 
+    // auth related api jwt generate
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          // secure: false,
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
+
+    // Clear token (on logout)
+    app.get("/logout", (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          maxAge: 0,
+        })
+        .send({ success: true });
+    });
+
     // all foods
     app.get("/foods", async (req, res) => {
+      // const search = req.query.search;
+      // // let query = {
+      // //   food_name: { $regex: search, $option: "i" },
+      // // };
       const result = await foodsCollection.find().toArray();
 
       res.send(result);
@@ -63,9 +121,20 @@ async function run() {
     // save order
     app.post("/order", async (req, res) => {
       const orderPlaced = req.body;
-      console.log(orderPlaced);
+      console.log("order", req.cookies);
+      const query = {
+        email: orderPlaced.email,
+        orderId: orderPlaced.orderId,
+        quantityLeft: orderPlaced.quantityLeft,
+      };
+      const alreadyOrdered = await orderCollection.findOne(query);
+      // if (alreadyOrdered) {
+      //   return res.status(400).send("You have already purchased");
+      // }
+      console.log(orderPlaced.email);
       const result = await orderCollection.insertOne(orderPlaced);
       res.send(result);
+      console.log(result);
     });
 
     // save food item
@@ -80,6 +149,10 @@ async function run() {
     // get food table by email
     app.get("/foods/:email", async (req, res) => {
       const email = req.params.email;
+      // const tokenEmail = req.user.email;
+      // if (tokenEmail !== email) {
+      //   return res.status(403).send({ message: "forbidden access" });
+      // }
       const query = { "buyer.email": email };
       const result = await foodsCollection.find(query).toArray();
       res.send(result);
@@ -122,22 +195,6 @@ async function run() {
       res.send(result);
     });
 
-    // auth related api jwt generate
-    app.post("/jwt", async (req, res) => {
-      const user = req.body;
-      console.log(user);
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "7d",
-      });
-      res
-        .cookie("token", token, {
-          httpOnly: true,
-          // secure: process.env.NODE_ENV === "production" ,
-          secure: false,
-          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-        })
-        .send({ success: true });
-    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
